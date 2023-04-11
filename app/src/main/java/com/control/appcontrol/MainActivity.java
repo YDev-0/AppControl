092a7dbcef720b747051952e20b21f92c7fcc189
YDev-0 <78972559+YDev-0@users.noreply.github.com>
2023-04-11 02:39:27 +0000

    commit v1

--- app/src/main/java/com/control/appcontrol/MainActivity.java
+++ app/src/main/java/com/control/appcontrol/MainActivity.java
@@ -1,14 +1,17 @@
 package com.control.appcontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar mainToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainToolbar = findViewById(R.id.mainToolbar);
+        setSupportActionBar(mainToolbar);
     }
 }
