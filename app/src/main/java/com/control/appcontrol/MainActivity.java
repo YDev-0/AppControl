d784fe5780d9c561976a198b09e25f1dad7f20b3
YDev-0 <78972559+YDev-0@users.noreply.github.com>
2023-04-11 03:41:54 +0000

    commit v1.1

--- app/src/main/java/com/control/appcontrol/MainActivity.java
+++ app/src/main/java/com/control/appcontrol/MainActivity.java
@@ -1,17 +1,34 @@
 package com.control.appcontrol;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
     private Toolbar mainToolbar;
+    private DrawerLayout mainDrawer;
+    private NavigationView mainMenu;
+    private ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mainToolbar = findViewById(R.id.mainToolbar);
         setSupportActionBar(mainToolbar);
+
+        mainDrawer = findViewById(R.id.mainDrawer);
+
+        mainMenu = findViewById(R.id.mainMenu);
+
+        toggle = new ActionBarDrawerToggle(this, mainDrawer, mainToolbar, R.string.open, R.string.close);
+
+        mainDrawer.addDrawerListener(toggle);
+
+        toggle.syncState();
     }
 }
