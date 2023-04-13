95d251b76c0a3628482dd53a0fabcf69835b9682
YDev-0 <78972559+YDev-0@users.noreply.github.com>
2023-04-13 04:00:09 +0000

    commit v1.4

--- app/src/main/java/com/control/appcontrol/ConvertFragment.java
+++ app/src/main/java/com/control/appcontrol/ConvertFragment.java
@@ -1,22 +1,31 @@
 package com.control.appcontrol;
 
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ConvertFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ConvertFragment extends Fragment {
+public class ConvertFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+
+    private Spinner currentDegreeSpinner;
+    private Spinner selectDegreeSpinner;
+    private EditText currentValue;
+    private EditText desiredValue;
+
+    private Button convertButton;
 
     public ConvertFragment() {
-        // Required empty public constructor
     }
 
     @Override
@@ -25,9 +34,62 @@ public class ConvertFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_convert, container, false);
+        currentValue = view.findViewById(R.id.currentValue);
+        desiredValue = view.findViewById(R.id.desiredValue);
+        convertButton = view.findViewById(R.id.convertButton);
+        currentDegreeSpinner = view.findViewById(R.id.current_degree_spinner);
+        selectDegreeSpinner = view.findViewById(R.id.select_degree_spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.spinner_values, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        currentDegreeSpinner.setAdapter(adapter);
+        selectDegreeSpinner.setAdapter(adapter);
+        currentDegreeSpinner.setOnItemSelectedListener(this);
+        selectDegreeSpinner.setOnItemSelectedListener(this);
+
+        convertButton.setOnClickListener(v -> {
+            String currentDegree = currentDegreeSpinner.getSelectedItem().toString();
+            String desiredDegree = selectDegreeSpinner.getSelectedItem().toString();
+            convert(currentDegree, desiredDegree);
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_convert, container, false);
+        return view;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String currentDegree = currentDegreeSpinner.getSelectedItem().toString();
+        String desiredDegree = selectDegreeSpinner.getSelectedItem().toString();
+        convertButton.setEnabled(!currentDegree.equals(desiredDegree));
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    private void convert(String currentDegree, String desiredDegree) {
+        double value = Float.parseFloat(currentValue.getText().toString());
+
+        switch(desiredDegree) {
+            case "C°": {
+                value = (currentDegree.equals("K°") ? (value - 273.15) : (((value - 32) * 5) / 9));
+                break;
+            }
+            case "F°": {
+                value = (currentDegree.equals("C°") ? (((value * 9) / 5) + 32) : ((((value - 32) * 5) / 9) + 373.15));
+                break;
+            }
+            case "K°": {
+                value = (currentDegree.equals("C°") ? (value + 273.15) : ((((value - 273.15) - 32) * 5) / 9));
+                break;
+            }
+        }
+
+        desiredValue.setText(Double.toString(value));
     }
 }
