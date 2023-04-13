f7aee412202721fded53353149e14a2afe617cf7
YDev-0 <maskirano7@gmail.com>
2023-04-13 10:19:27 +0000

    commit 1.4.1

--- app/src/main/java/com/control/appcontrol/ConvertFragment.java
+++ app/src/main/java/com/control/appcontrol/ConvertFragment.java
@@ -74,23 +74,23 @@ public class ConvertFragment extends Fragment implements AdapterView.OnItemSelec
     }
 
     private void convert(String currentDegree, String desiredDegree) {
-        double value = Float.parseFloat(currentValue.getText().toString());
+        double value = Double.parseDouble(currentValue.getText().toString());
 
         switch(desiredDegree) {
             case "C°": {
-                value = (currentDegree.equals("K°") ? (value - 273.15) : (((value - 32) * 5) / 9));
+                value = (currentDegree.equals("K°") ? (value - 273.15) : ((value - 32) * 5 / 9));
                 break;
             }
             case "F°": {
-                value = (currentDegree.equals("C°") ? (((value * 9) / 5) + 32) : ((((value - 32) * 5) / 9) + 373.15));
+                value = (currentDegree.equals("C°") ? ((value * 9 / 5) + 32) : (((value - 273.15) * 9 / 5) + 32));
                 break;
             }
             case "K°": {
-                value = (currentDegree.equals("C°") ? (value + 273.15) : ((((value - 273.15) - 32) * 5) / 9));
+                value = (currentDegree.equals("C°") ? (value + 273.15) : (((value - 32) * 5 / 9) + 273.15));
                 break;
             }
         }
 
-        desiredValue.setText(Double.toString(value));
+        desiredValue.setText("" + value);
     }
 }
