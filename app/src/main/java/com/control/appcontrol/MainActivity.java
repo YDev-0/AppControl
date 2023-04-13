0491725b992d9bb0b16073059fcabc32dc80d532
YDev-0 <78972559+YDev-0@users.noreply.github.com>
2023-04-13 00:27:48 +0000

    commit v1.3

--- app/src/main/java/com/control/appcontrol/MainActivity.java
+++ app/src/main/java/com/control/appcontrol/MainActivity.java
@@ -3,6 +3,7 @@ package com.control.appcontrol;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
@@ -18,17 +19,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mainToolbar = findViewById(R.id.mainToolbar);
-        setSupportActionBar(mainToolbar);
-
         mainDrawer = findViewById(R.id.mainDrawer);
-
         mainMenu = findViewById(R.id.mainMenu);
-
+        setSupportActionBar(mainToolbar);
         toggle = new ActionBarDrawerToggle(this, mainDrawer, mainToolbar, R.string.open, R.string.close);
-
         mainDrawer.addDrawerListener(toggle);
-
         toggle.syncState();
+
+        mainMenu.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.mainFrameLayout, new HomeFragment()).commit();
+                    mainDrawer.closeDrawer(GravityCompat.START);
+                    break;
+
+                case R.id.convert:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.mainFrameLayout, new ConvertFragment()).commit();
+                    mainDrawer.closeDrawer(GravityCompat.START);
+                    break;
+
+                case R.id.info:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.mainFrameLayout, new AboutFragment()).commit();
+                    mainDrawer.closeDrawer(GravityCompat.START);
+                    break;
+            }
+
+            return true;
+        });
     }
 }
